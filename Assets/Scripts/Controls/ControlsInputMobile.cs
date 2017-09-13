7aced0c67d964c6e25555ae70d1e76bdd31acb76
Muhammad Farhan <farhan.aqeel@hotmail.com>
2017-09-13 20:45:34 +0500

    Added mobile controls

---
+++ Assets/Scripts/Controls/ControlsInputMobile.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+
+	public class ControlsInputMobile : ControlsBase {
+
+	Vector2 _movement = Vector2.zero;
+
+
+	void Update(){
+		
+		_movement.x = CrossPlatformInputManager.GetAxis("Horizontal");
+		_movement.y = CrossPlatformInputManager.GetAxis("Vertical");
+
+			controllable.Move(_movement);
+//		controllable.Attacking = Input.GetButton("Fire2");
+		controllable.Attacking = _fire;
+
+//		controllable.Protecting = Input.GetButton("Fire3");
+		controllable.Protecting = _shield;
+	}
+
+
+	bool _fire = false;
+	bool _shield = false;
+
+	public void Fire(){
+		_fire = !_fire;
+	}
+
+	public void Shield(){
+		_shield = !_shield;
+	}
+
+}
